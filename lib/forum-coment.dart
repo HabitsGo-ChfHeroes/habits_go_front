249e38716302e0801138582d6efa493866cd696d
NotLT42 <u20211c667@upc.edu.pe>
2025-05-06 02:21:43 -0500

    Feat: Creating forum profiles views

---
+++ lib/forum-coment.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/material.dart';
+
+class ForumComentPage extends StatelessWidget {
+  final String name;
+  final String description;
+  final String email;
+
+  const ForumComentPage({
+    super.key,
+    required this.name,
+    required this.description,
+    required this.email,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back),
+          onPressed: () => Navigator.pop(context),
+        ),
+        title: const Text('Perfil de Usuario'),
+        backgroundColor: const Color(0xFF003A73),
+        foregroundColor: Colors.white,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          children: [
+            const CircleAvatar(
+                radius: 60,
+                backgroundColor: Colors.grey,
+                backgroundImage: AssetImage("assets/gatowa.jpg")
+              //child: Icon(Icons.person, size: 60, color: Colors.white),
+            ),
+            const SizedBox(height: 16),
+            Text(
+              name,
+              style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 16),
+            Expanded(
+              child: SingleChildScrollView(
+                child: Text(
+                  description,
+                  style: const TextStyle(fontSize: 16),
+                  textAlign: TextAlign.justify,
+                ),
+              ),
+            ),
+            const SizedBox(height: 10),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                const SizedBox(width: 15),
+                const Icon(Icons.email, color: Colors.grey),
+                Expanded(
+                  child: Text(
+                    email,
+                    style: const TextStyle(fontSize: 16),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 20),
+            SizedBox(
+              width: double.infinity,
+              child: ElevatedButton.icon(
+                icon: const Icon(Icons.download, color: Colors.white),
+                label: const Text('Guardar', style: TextStyle(color: Colors.white)),
+                onPressed: () {
+                  // Acción de guardar
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: const Color(0xFF007BFF),
+                  padding: const EdgeInsets.symmetric(vertical: 14),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
