24f4af8bf6e3bc2635fc33a29a6e62d63a92c437
Aldo Francisco Pastrana Leon <102763835+U20211C186@users.noreply.github.com>
2025-05-11 10:54:28 -0500

    Actualizando lo mas simil al figma

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -15,25 +15,23 @@ class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(
+      
+      backgroundColor: Colors.white,
+      body: Center(  // Centra el contenido
+        child: SingleChildScrollView(  // Envuelve el contenido en un SingleChildScrollView
+          padding: const EdgeInsets.all(10.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,  // Centra verticalmente
+            crossAxisAlignment: CrossAxisAlignment.center,  // Centra horizontalmente
+            children: [
+              Text(
           'Bienvenido!',
           style: TextStyle(
             fontSize: 26, // Tamaño más grande
             fontWeight: FontWeight.bold, // Negrita
           ),
         ),
-        backgroundColor: Colors.white, // Azul
-        centerTitle: true, // Centra el título
-      ),
-      body: SingleChildScrollView(  // Envuelve el contenido en un SingleChildScrollView
-        padding: const EdgeInsets.all(10.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Center(
-              child: Text(
+              Text(
                 'Inicia Sesión para Continuar',
                 style: TextStyle(
                   fontSize: 16, // Tamaño pequeño
@@ -41,183 +39,183 @@ class _LoginScreenState extends State<LoginScreen> {
                   color: Colors.grey[700], // Gris suave
                 ),
               ),
-            ),
-            
-            SizedBox(height: 60),
-            // Campo de email
-            TextField(
-              controller: _emailController,
-              decoration: InputDecoration(
-                labelText: 'Email',
-                border: OutlineInputBorder(),
-                prefixIcon: Icon(Icons.email),
-              ),
-            ),
-            SizedBox(height: 20),
-            // Campo de contraseña
-            TextField(
-              controller: _passwordController,
-              obscureText: true,
-              decoration: InputDecoration(
-                labelText: 'Contraseña',
-                border: OutlineInputBorder(),
-                prefixIcon: Icon(Icons.lock),
-                suffixIcon: Icon(Icons.visibility_off),
-              ),
-            ),
-            SizedBox(height: 10),
-            // Recuérdame y Olvidaste la Contraseña
-            Row(
-              children: [
-                Checkbox(
-                  value: _rememberMe,
-                  onChanged: (bool? value) {
-                    setState(() {
-                      _rememberMe = value!;
-                    });
-                  },
+              SizedBox(height: 80),
+              // Campo de email
+              TextField(
+                controller: _emailController,
+                decoration: InputDecoration(
+                  labelText: 'Email',
+                  border: OutlineInputBorder(),
+                  prefixIcon: Icon(Icons.email),
                 ),
-                Text(
-                  'Recuérdame',
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold, // Negrita
-                    color: Colors.black, // Color negro
+              ),
+              SizedBox(height: 20),
+              // Campo de contraseña
+              TextField(
+                controller: _passwordController,
+                obscureText: true,
+                decoration: InputDecoration(
+                  labelText: 'Contraseña',
+                  border: OutlineInputBorder(),
+                  prefixIcon: Icon(Icons.lock),
+                  suffixIcon: Icon(Icons.visibility_off),
+                ),
+              ),
+              SizedBox(height: 10),
+              // Recuérdame y Olvidaste la Contraseña
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,  // Alineación a la izquierda
+                children: [
+                  Checkbox(
+                    value: _rememberMe,
+                    onChanged: (bool? value) {
+                      setState(() {
+                        _rememberMe = value!;
+                      });
+                    },
                   ),
-                ),
-                Spacer(),
-                TextButton(
-                  onPressed: () {},
-                  child: Text(
-                    'Olvidaste la Contraseña?',
+                  Text(
+                    'Recuérdame',
                     style: TextStyle(
                       fontWeight: FontWeight.bold, // Negrita
                       color: Colors.black, // Color negro
                     ),
                   ),
-                ),
-              ],
-            ),
-            SizedBox(height: 40),
-            // Botón de Iniciar Sesión
-            SizedBox(
-              width: double.infinity, // Hace que el botón ocupe todo el ancho de la pantalla
-              child: ElevatedButton(
-                onPressed: () {
-                  Navigator.of(context).pushNamedAndRemoveUntil(
-                    "progress",
-                    (Route<dynamic> route) => false,
-                  );
-                },
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Color(0xFF226980), // Color de fondo del botón
-                  padding: EdgeInsets.symmetric(vertical: 15), // Solo ajustamos el padding vertical
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10), // Bordes redondeados más suaves
-                  ),
-                  elevation: 5, // Sombra para dar un efecto más realista
-                ),
-                child: Text(
-                  'Iniciar Sesión',
-                  style: TextStyle(
-                    fontSize: 18,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.white,
-                  ),
-                ),
-              ),
-            ),
-            SizedBox(height: 60),
-            Row(
-              children: [
-                Expanded(
-                  child: Divider(
-                    color: Colors.grey[400], // Color de la línea (gris claro)
-                    thickness: 1, // Grosor de la línea
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 8.0), // Espaciado entre las líneas
-                  child: Text(
-                    'Iniciar sesión con',
-                    style: TextStyle(
-                      fontSize: 16, // Tamaño de fuente
-                      color: Colors.grey[600], // Color gris suave
-                    ),
-                  ),
-                ),
-                Expanded(
-                  child: Divider(
-                    color: Colors.grey[400], // Color de la línea
-                    thickness: 1, // Grosor de la línea
-                  ),
-                ),
-              ],
-            ),
-            SizedBox(height: 10),
-            // Iconos de inicio de sesión con Google, Facebook y Apple
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                // Google icon
-                IconButton(
-                  icon: Image.asset(
-                    'assets/google.png',
-                    width: 35,  // Ajusta el tamaño del icono
-                    height: 35, // Ajusta el tamaño del icono
-                  ),
-                  onPressed: () {},
-                ),
-                SizedBox(width: 30),
-                // Facebook icon
-                IconButton(
-                  icon: Image.asset(
-                    'assets/facebook.png',
-                    width: 35,  // Ajusta el tamaño del icono
-                    height: 35, // Ajusta el tamaño del icono
-                  ),
-                  onPressed: () {},
-                ),
-                SizedBox(width: 30),
-                // Apple icon
-                IconButton(
-                  icon: Image.asset(
-                    'assets/apple.png',
-                    width: 35,  // Ajusta el tamaño del icono
-                    height: 35, // Ajusta el tamaño del icono
-                  ),
-                  onPressed: () {},
-                ),
-              ],
-            ),
-            SizedBox(height: 10),
-            // Enlace para registro
-            Center(
-              child: TextButton(
-                onPressed: () {
-                  Navigator.of(context).pushNamed("register");
-                },
-                child: RichText(
-                  text: TextSpan(
-                    text: 'Sin Cuenta? ', // Texto "Sin Cuenta?" en gris
-                    style: TextStyle(
-                      color: Colors.grey[600], // Gris
-                      fontSize: 16, // Tamaño de fuente adecuado
-                    ),
-                    children: <TextSpan>[
-                      TextSpan(
-                        text: 'Regístrate Aquí', // Parte en color azul
-                        style: TextStyle(
-                          color: Color(0xFF226980), // Color azul
-                          fontSize: 16, // Tamaño de fuente
-                          fontWeight: FontWeight.bold, // Negrita para resaltar
-                        ),
+                  Spacer(),
+                  TextButton(
+                    onPressed: () {},
+                    child: Text(
+                      'Olvidaste la Contraseña?',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold, // Negrita
+                        color: Colors.black, // Color negro
                       ),
-                    ],
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: 40),
+              // Botón de Iniciar Sesión
+              SizedBox(
+                width: double.infinity, // Hace que el botón ocupe todo el ancho de la pantalla
+                child: ElevatedButton(
+                  onPressed: () {
+                    Navigator.of(context).pushNamedAndRemoveUntil(
+                      "progress",
+                      (Route<dynamic> route) => false,
+                    );
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Color(0xFF226980), // Color de fondo del botón
+                    padding: EdgeInsets.symmetric(vertical: 15), // Solo ajustamos el padding vertical
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10), // Bordes redondeados más suaves
+                    ),
+                    elevation: 5, // Sombra para dar un efecto más realista
+                  ),
+                  child: Text(
+                    'Iniciar Sesión',
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
                   ),
                 ),
               ),
-            )
-          ],
+              SizedBox(height: 60),
+              Row(
+                children: [
+                  Expanded(
+                    child: Divider(
+                      color: Color(0xFF2D2D2D), // Color de la línea (gris claro)
+                      thickness: 1, // Grosor de la línea
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 8.0), // Espaciado entre las líneas
+                    child: Text(
+                      'Iniciar sesión con',
+                      style: TextStyle(
+                        fontSize: 16, // Tamaño de fuente
+                        color: Colors.grey[600], // Color gris suave
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: Divider(
+                      color: Color(0xFF2D2D2D), // Color de la línea
+                      thickness: 1, // Grosor de la línea
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: 10),
+              // Iconos de inicio de sesión con Google, Facebook y Apple
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  // Google icon
+                  IconButton(
+                    icon: Image.asset(
+                      'assets/google.png',
+                      width: 35,  // Ajusta el tamaño del icono
+                      height: 35, // Ajusta el tamaño del icono
+                    ),
+                    onPressed: () {},
+                  ),
+                  SizedBox(width: 30),
+                  // Facebook icon
+                  IconButton(
+                    icon: Image.asset(
+                      'assets/facebook.png',
+                      width: 40,  // Ajusta el tamaño del icono
+                      height: 40, // Ajusta el tamaño del icono
+                    ),
+                    onPressed: () {},
+                  ),
+                  SizedBox(width: 30),
+                  // Apple icon
+                  IconButton(
+                    icon: Image.asset(
+                      'assets/apple.png',
+                      width: 35,  // Ajusta el tamaño del icono
+                      height: 35, // Ajusta el tamaño del icono
+                    ),
+                    onPressed: () {},
+                  ),
+                ],
+              ),
+              SizedBox(height: 60),
+              // Enlace para registro
+              Center(
+                child: TextButton(
+                  onPressed: () {
+                    Navigator.of(context).pushNamed("register");
+                  },
+                  child: RichText(
+                    text: TextSpan(
+                      text: 'Sin Cuenta? ', // Texto "Sin Cuenta?" en gris
+                      style: TextStyle(
+                        color: Colors.grey[600], // Gris
+                        fontSize: 16, // Tamaño de fuente adecuado
+                      ),
+                      children: <TextSpan>[
+                        TextSpan(
+                          text: 'Regístrate Aquí', // Parte en color azul
+                          style: TextStyle(
+                            color: Color(0xFF226980), // Color azul
+                            fontSize: 16, // Tamaño de fuente
+                            fontWeight: FontWeight.bold, // Negrita para resaltar
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
--- lib/screens/register_screen.dart
+++ lib/screens/register_screen.dart
@@ -8,140 +8,141 @@ class RegisterScreen extends StatefulWidget {
 }
 
 class _RegisterState extends State<RegisterScreen> {
+    final _nombresController = TextEditingController();
+  final _apellidosController = TextEditingController();
   final _emailController = TextEditingController();
+    final _telefonocontroller = TextEditingController();
+    final _direccioncontroller = TextEditingController();
+
   final _passwordController = TextEditingController();
+  final _repeatpasswordController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          'Registro',
-          style: TextStyle(
-            fontSize: 26, // Tamaño más grande
-            fontWeight: FontWeight.bold, // Negrita
+      
+      backgroundColor: Colors.white,
+
+      body: Center(  // Centra todo el contenido
+        child: SingleChildScrollView(
+          padding: const EdgeInsets.all(10.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,  // Centra verticalmente
+            crossAxisAlignment: CrossAxisAlignment.center,  // Centra horizontalmente
+            children: [
+              SizedBox(height: 10),
+              // Campo de Nombres
+              TextField(
+                controller: _nombresController,
+                decoration: InputDecoration(
+                  labelText: 'Nombres',
+                  border: OutlineInputBorder(),
+                                      filled: true,  // Habilita el color de fondo
+    fillColor: Colors.grey.shade200,  // Cambia el color de fondo aquí
+                ),
+              ),
+              SizedBox(height: 20),
+              // Campo de Apellidos
+              TextField(
+                controller: _apellidosController,
+                decoration: InputDecoration(
+                  labelText: 'Apellidos',
+                  border: OutlineInputBorder(),
+                                      filled: true,  // Habilita el color de fondo
+    fillColor: Colors.grey.shade200,  // Cambia el color de fondo aquí
+                ),
+              ),
+              SizedBox(height: 20),
+              // Campo de Email
+              TextField(
+                controller: _emailController,
+                decoration: InputDecoration(
+                  labelText: 'Email',
+                  border: OutlineInputBorder(),
+                                      filled: true,  // Habilita el color de fondo
+    fillColor: Colors.grey.shade200,  // Cambia el color de fondo aquí
+                ),
+              ),
+              SizedBox(height: 20),
+              // Campo de Teléfono
+              TextField(
+                controller: _telefonocontroller,
+                decoration: InputDecoration(
+                  labelText: 'Teléfono',
+                  border: OutlineInputBorder(),
+                     filled: true,  // Habilita el color de fondo
+    fillColor: Colors.grey.shade200,  // Cambia el color de fondo aquí
+                ),
+              ),
+              SizedBox(height: 20),
+              // Campo de Dirección
+              TextField(
+                controller: _direccioncontroller,
+                decoration: InputDecoration(
+                  labelText: 'Dirección',
+                  border: OutlineInputBorder(),
+                                      filled: true,  // Habilita el color de fondo
+    fillColor: Colors.grey.shade200,  // Cambia el color de fondo aquí
+                ),
+              ),
+              SizedBox(height: 20),
+              // Campo de Contraseña
+              TextField(
+                controller: _passwordController,
+                obscureText: true,
+                decoration: InputDecoration(
+                  labelText: 'Contraseña',
+                  border: OutlineInputBorder(),
+                  suffixIcon: Icon(Icons.visibility_off),
+                                      filled: true,  // Habilita el color de fondo
+    fillColor: Colors.grey.shade200,  // Cambia el color de fondo aquí
+                ),
+              ),
+              SizedBox(height: 20),
+              // Campo de Repetir Contraseña
+              TextField(
+                controller: _repeatpasswordController,
+                obscureText: true,
+                decoration: InputDecoration(
+                  labelText: 'Contraseña',
+                  border: OutlineInputBorder(),
+                  suffixIcon: Icon(Icons.visibility_off),
+                                      filled: true,  // Habilita el color de fondo
+    fillColor: Colors.grey.shade200,  // Cambia el color de fondo aquí
+                ),
+              ),
+              SizedBox(height: 60),
+              // Botón de Registrarse
+              SizedBox(
+                width: double.infinity,  // Hace que el botón ocupe todo el ancho de la pantalla
+                child: ElevatedButton(
+                  onPressed: () {
+                    Navigator.of(context).pushNamedAndRemoveUntil(
+                      "user_imc_goal",
+                      (Route<dynamic> route) => false,
+                    );
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Color(0xFF226980), // Color de fondo del botón
+                    padding: EdgeInsets.symmetric(vertical: 15), // Solo ajustamos el padding vertical
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10), // Bordes redondeados más suaves
+                    ),
+                    elevation: 5, // Sombra para dar un efecto más realista
+                  ),
+                  child: Text(
+                    'Registrarse',
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: 10),
+            ],
           ),
-        ),
-        backgroundColor: Colors.white, // Azul
-        centerTitle: true, // Centra el título
-      ),
-      body: SingleChildScrollView(
-        // Envuelve el contenido en un SingleChildScrollView
-        padding: const EdgeInsets.all(10.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            SizedBox(height: 10),
-            // Campo de email
-            TextField(
-              controller: _emailController,
-              decoration: InputDecoration(
-                labelText: 'Nombres',
-                border: OutlineInputBorder(),
-                //prefixIcon: Icon(Icons.email),
-              ),
-            ),
-                        SizedBox(height: 20),
-            TextField(
-              controller: _emailController,
-              decoration: InputDecoration(
-                labelText: 'Apellidos',
-                border: OutlineInputBorder(),
-                //prefixIcon: Icon(Icons.email),
-              ),
-            ),
-            SizedBox(height: 20),
-            TextField(
-              controller: _emailController,
-              decoration: InputDecoration(
-                labelText: 'Email',
-                border: OutlineInputBorder(),
-                //prefixIcon: Icon(Icons.email),
-              ),
-            ),
-            SizedBox(height: 20),
-            TextField(
-              controller: _emailController,
-              decoration: InputDecoration(
-                labelText: 'Telefono',
-                border: OutlineInputBorder(),
-                //prefixIcon: Icon(Icons.email),
-              ),
-            ),
-            SizedBox(height: 20),
-            TextField(
-              controller: _emailController,
-              decoration: InputDecoration(
-                labelText: 'Direccion',
-                border: OutlineInputBorder(),
-                //prefixIcon: Icon(Icons.email),
-              ),
-            ),
-            SizedBox(height: 20),
-            // Campo de contraseña
-            TextField(
-              controller: _passwordController,
-              obscureText: true,
-              decoration: InputDecoration(
-                labelText: 'Contraseña',
-                border: OutlineInputBorder(),
-                //prefixIcon: Icon(Icons.lock),
-                suffixIcon: Icon(Icons.visibility_off),
-              ),
-            ),
-            SizedBox(height: 20),
-            // Campo de contraseña
-            TextField(
-              controller: _passwordController,
-              obscureText: true,
-              decoration: InputDecoration(
-                labelText: 'Repetir contraseña',
-                border: OutlineInputBorder(),
-                //prefixIcon: Icon(Icons.lock),
-                suffixIcon: Icon(Icons.visibility_off),
-              ),
-            ),            // Recuérdame y Olvidaste la Contraseña
-            SizedBox(height: 20),
-            // Botón de Iniciar Sesión
-            SizedBox(
-              width:
-                  double
-                      .infinity, // Hace que el botón ocupe todo el ancho de la pantalla
-              child: ElevatedButton(
-                onPressed: () {
-                  Navigator.of(context).pushNamedAndRemoveUntil(
-                    "user_imc_goal",
-                    (Route<dynamic> route) => false,
-                  );
-                },
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Color(
-                    0xFF226980,
-                  ), // Color de fondo del botón
-                  padding: EdgeInsets.symmetric(
-                    vertical: 15,
-                  ), // Solo ajustamos el padding vertical
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(
-                      10,
-                    ), // Bordes redondeados más suaves
-                  ),
-                  elevation: 5, // Sombra para dar un efecto más realista
-                ),
-                child: Text(
-                  'Registrarse',
-                  style: TextStyle(
-                    fontSize: 18,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.white,
-                  ),
-                ),
-              ),
-            ),
-            SizedBox(height: 10),
-            
-          ],
         ),
       ),
     );
