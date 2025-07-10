1fd5a449093d52261b0db45815a93daaeebfeabf
Sebastian Ramirez <89022108+SebastianRamirez-16@users.noreply.github.com>
2025-07-09 19:57:18 -0500

    Add diet advice and local notifications

--- lib/main.dart
+++ lib/main.dart
@@ -15,9 +15,11 @@ import 'package:habits_go_front/screens/daily_plan_screen.dart';
 import 'package:habits_go_front/screens/daily_plan_loading.dart';
 import 'package:habits_go_front/screens/favorite_foods_screen.dart';
 import 'package:habits_go_front/screens/habits_questionnaire_screen.dart';
+import 'package:habits_go_front/services/notification_service.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
+  NotificationService.initialize();
   SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersiveSticky);
   runApp(MultiProvider(
       providers: [
@@ -28,8 +30,32 @@ void main() {
   );
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends StatefulWidget {
   const MainApp({super.key});
+
+  @override
+  State<MainApp> createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> with WidgetsBindingObserver {
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addObserver(this);
+  }
+
+  @override
+  void dispose() {
+    WidgetsBinding.instance.removeObserver(this);
+    super.dispose();
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    if (state == AppLifecycleState.paused) {
+      NotificationService.showExitReminder();
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
